4e4e9388207a7b22f25f6b62e7de64bc0293da4b
Mohamed Shaheer <shaheer@coderythm.com>
2023-11-20 22:24:09 +0530

    fix: header and order content page fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,226 +1,188 @@
-import { Box, Typography, Grid } from '@mui/material';
-import { styled } from '@mui/material';
-import React, { useState } from 'react'
-import logoImg from '../../assets/logoImg.svg';
-import followImage from '../../assets/followicon.png'
-import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
-import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
-import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
-import ContactsIcon from '@mui/icons-material/Contacts';
+import { Box, Typography, Grid } from "@mui/material";
+import { styled } from "@mui/material";
+import React, { useState } from "react";
+import logoImg from "../../assets/logoImg.svg";
+import followImage from "../../assets/followicon.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
+import ContactsIcon from "@mui/icons-material/Contacts";
 import {
   Drawer,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
-  ListItemText
-
-} from "@mui/material"
-
+  ListItemText,
+} from "@mui/material";
+import "../../css/header.css";
 
 const Header = () => {
-
-  const [mobileMenu, setMobileMenu] = useState({ right: false })
+  const [mobileMenu, setMobileMenu] = useState({ right: false });
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.type === 'Tab' || event.type === 'shift')) {
-      return
+    if (
+      event.type === "keydown" &&
+      (event.type === "Tab" || event.type === "shift")
+    ) {
+      return;
     }
-    setMobileMenu({ ...mobileMenu, [anchor]: open })
-  }
+    setMobileMenu({ ...mobileMenu, [anchor]: open });
+  };
 
   const list = ({ anchor }) => {
     return (
-      <div style={{
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center'
-      }}>
-
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
         <List>
-          {
-            nav_title.map((item, index) => {
-              return (
-                <ListItem key={item.index} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      {
-                        index === 0 && <HomeIcon />
-                      }
-                      {
-                        index === 1 && <FeaturedPlayListIcon />
-                      }
-                      {
-                        index === 2 && <MiscellaneousServicesIcon />
-                      }
-                      {
-                        index === 3 && <ContactsIcon />
-                      }
-                    </ListItemIcon>
-                    <ListItemText primary={item.display} />
-                  </ListItemButton>
-                </ListItem>
-              )
-            })
-          }
+          {nav_title.map((item, index) => {
+            return (
+              <ListItem key={item.index} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index === 0 && <HomeIcon />}
+                    {index === 1 && <FeaturedPlayListIcon />}
+                    {index === 2 && <MiscellaneousServicesIcon />}
+                    {index === 3 && <ContactsIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={item.display} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
         </List>
-
       </div>
+    );
+  };
 
-    )
-  }
-
-  const nav_title = [{
-    path: '/',
-    display: 'Home'
-  },
-  {
-    path: '/service',
-    display: 'Services'
-  },
-  {
-    path: '/',
-    display: 'About Us'
-  },
-  {
-    path: '/',
-    display: 'Add a store'
-  },
-  ]
-  const NavBarLinksBox = styled(Box)(({ theme }) => ({
-    display: 'flex',
-    justifyContent: 'initial',
-    gap: '38px',
-    [theme.breakpoints.down("md")]: {
-      display: 'none'
+  const nav_title = [
+    {
+      path: "/",
+      display: "Home",
     },
-  }))
+    {
+      path: "/service",
+      display: "Services",
+    },
+    {
+      path: "/",
+      display: "About Us",
+    },
+    {
+      path: "/",
+      display: "Add a store",
+    },
+  ];
+  const NavBarLinksBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "initial",
+    gap: "38px",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
 
   const NavBarLink = styled(Typography)(() => ({
-    fontSize: '13px',
-    fontWeight: '700',
-    position: 'relative',
-    lineHeight: 'normal',
-    fontFamily: 'Inter',
-    cursor: 'pointer',
-    fontStyle: 'normal',
-    color: '#FFF',
-    '&:hover::before': {
-      fontSize: '33px',
-      fontWeight: '10000',
+    fontSize: "13px",
+    fontWeight: "700",
+    position: "relative",
+    lineHeight: "normal",
+    fontFamily: "Inter",
+    cursor: "pointer",
+    fontStyle: "normal",
+    color: "#FFF",
+    "&:hover::before": {
+      fontSize: "33px",
+      fontWeight: "10000",
       content: '""',
-      position: 'absolute',
-      width: '61px',
-      height: '3px',
-      marginTop: '33px',
-      marginLeft: '-7px',
+      position: "absolute",
+      width: "61px",
+      height: "3px",
+      marginTop: "33px",
+      marginLeft: "-7px",
       flexShrink: 0,
-      borderRadius: '12px 12px 0px 0px',
-      background: '#FFF'
+      borderRadius: "12px 12px 0px 0px",
+      background: "#FFF",
     },
-    '&:hover': {
+    "&:hover": {
       '&:contains("Add a store")': {
-        '&::before': {
-          width: '84px',
-          height: '3px',
+        "&::before": {
+          width: "84px",
+          height: "3px",
         },
       },
       '&:contains("About Us")': {
-        '&::before': {
-          width: '100px',
+        "&::before": {
+          width: "100px",
         },
       },
       '&:contains("Follow Us on")': {
-        '&::before': {
-          width: '120px',
+        "&::before": {
+          width: "120px",
         },
       },
     },
-  }))
+  }));
   const FollowIcon = styled("img")(() => ({
-    width: '9px',
-    height: '9px',
-    flexShrink: '0'
-  }))
+    width: "9px",
+    height: "9px",
+    flexShrink: "0",
+  }));
 
   const NavBarLogo = styled("img")(({ theme }) => ({
-    cursor: 'pointer',
-    width: '115px',
-    height: '35px',
-    display: 'block',
-    objectFit: 'contain',
+    cursor: "pointer",
+    width: "115px",
+    height: "35px",
+    display: "block",
+    objectFit: "contain",
     [theme.breakpoints.down("md")]: {
       display: "none",
     },
   }));
   const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
-    cursor: 'pointer',
-    display: 'none',
-    color: 'white',
+    cursor: "pointer",
+    display: "none",
+    color: "white",
     marginRight: theme.spacing(5),
     [theme.breakpoints.down("md")]: {
       display: "block",
     },
   }));
   return (
-    <Box sx={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: '20px',
-      maxWidth: 'auto',
-      marginLeft: '0px',
-      marginBottom: '20px',
-      backgroundColor: '#0E0B34',
-
-    }}>
-      <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '4.5rem',
-        marginLeft: '2rem'
-      }}>
-        <Box sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
+    <div className="header-main-div">
+      <div className="header-content-left">
+        <div className="header-content-left-drawer-div">
+          {" "}
           <CustomMenuIcon onClick={toggleDrawer("right", true)} />
-          <Drawer anchor="right"
+          <Drawer
+            anchor="right"
             open={mobileMenu["right"]}
-            onClose={toggleDrawer("right", false)}>
+            onClose={toggleDrawer("right", false)}
+          >
             {list("right")}
           </Drawer>
           <NavBarLogo src={logoImg} />
-        </Box>
+        </div>
+
         <NavBarLinksBox>
           {nav_title.map((item, index) => {
-            return (
-              <NavBarLink key={index}>{item.display}</NavBarLink>
-            )
-
+            return <NavBarLink key={index}>{item.display}</NavBarLink>;
           })}
         </NavBarLinksBox>
+      </div>
 
-      </Box>
-      <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '0.8rem',
-        marginRight: '38px',
-        order: [1, 0],
-
-      }}>
+      <div className="header-content-right">
         <FollowIcon src={followImage} />
-        <NavBarLink>
-          Follow us on
-        </NavBarLink>
-      </Box>
-    </Box>
-  )
-}
+        <NavBarLink>Follow us on</NavBarLink>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/Pages/OrderContent/Order_content.jsx
+++ src/Pages/OrderContent/Order_content.jsx
@@ -1,179 +1,151 @@
-import {styled} from '@mui/material';
-import { Box,Typography } from '@mui/material'
-import Grid from '@mui/material/Grid';
-import React from 'react'
-   
-const OrderTextBox = styled(Box)(()=>({
-  display:'flex',
-  gap:'2rem',
-  marginTop:'5%',
+import { styled } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import React from "react";
+import "../../css/orderContent.css";
+const OrderTextBox = styled(Box)(() => ({
+  display: "flex",
+  gap: "2rem",
+  marginTop: "5%",
+}));
 
-}))
+const OrderContent = styled(Box)(() => ({}));
 
-const OrderContent = styled(Box)(()=>({
-
-}))
-
-const OrderTitle = styled(Typography)(()=>({
-  fontFamily:'Inter',
-  fontSize:'16px',
-  fontStyle:'normal',
-  fontWeight:'600',
-  lineHeight:'normal',
-}))
-const NumberText = styled(Box)(()=>({
-  color: '#120A6C',
-  fontFamily:'Inter',
-  fontSize:'50px',
-  fontStyle:'normal',
-  fontWeight:'800',
-  lineHeight:'normal',
-}))
+const OrderTitle = styled(Typography)(() => ({
+  fontFamily: "Inter",
+  fontSize: "16px",
+  fontStyle: "normal",
+  fontWeight: "600",
+  lineHeight: "normal",
+}));
+const NumberText = styled(Box)(() => ({
+  color: "#120A6C",
+  fontFamily: "Inter",
+  fontSize: "50px",
+  fontStyle: "normal",
+  fontWeight: "800",
+  lineHeight: "normal",
+}));
 
 const Order_content = () => {
   return (
-    <Box style={{width: '100%',  height:'342px' , background: '#FFFCFC',}}
-    sx={{
-       width:'100%',
-      marginTop:'52px'
-    }} >
-      <Box sx={{
-        marginLeft:'7rem'
-      }}>
+    // <Box
+    //   style={{ width: "100%", height: "342px", background: "#FFFCFC" }}
+    //   sx={{
+    //     width: "100%",
+    //     marginTop: "52px",
+    //   }}
+    // >
+    <div className="order-content-main-div">
+      <div className="order-content-div">
+        <div className="order-content-heading">
+          <OrderTitle>What you need to do to order</OrderTitle>
+        </div>
+        <div className="order-content-items-div">
+          <OrderTextBox>
+            <NumberText>01</NumberText>
+            <OrderContent>
+              <Typography
+                sx={{
+                  color: "#08005F",
 
-       <Box sx={{
-           background:'linear-gradient(90deg, #1F168B 4.12%, rgba(200, 53, 106, 0.96) 85.37%)',
-           backgroundClip:'text',
-           WebkitBackgroundClip:'text',
-           WebkitTextFillColor:'transparent'
-       }}>
-        <OrderTitle >
-        What you need to do to order
-        </OrderTitle>
-       </Box>
-       <Box sx={{display:'flex', justifyContent:'space-between'}}>
-       <OrderTextBox>
-        <NumberText>
-          01
-        </NumberText>
-        <OrderContent>
-          <Typography sx={{color: '#08005F',
+                  fontFamily: "Inter",
+                  fontSize: "22px",
+                  fontStyle: "normal",
+                  fontWeight: "600",
+                  lineHeight: "normal",
+                }}
+              >
+                Select your Store
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#828282",
+                  marginTop: "5%",
+                  fontFamily: "Questrial",
+                  fontSize: "14px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "166.023%",
+                }}
+              >
+                from your morning coffee needs to snacks
+                <br /> for your late night binge.
+              </Typography>
+            </OrderContent>
+          </OrderTextBox>
 
-fontFamily: 'Inter',
-fontSize: '22px',
-fontStyle: 'normal',
-fontWeight: '600',
-lineHeight: 'normal',}}>
-          Select your Store
-          </Typography>
-          <Typography sx={{color: '#828282',
-          marginTop:'5%',
-    fontFamily:'Questrial',
-    fontSize:'14px',
-    fontStyle:'normal',
-    fontWeight:'400',
-    lineHeight:'166.023%',}}>
-          from your morning coffee needs to snacks<br/> for your late night binge.
-          </Typography>
-        </OrderContent>
-       </OrderTextBox>
+          <OrderTextBox>
+            <NumberText>01</NumberText>
+            <OrderContent>
+              <Typography
+                sx={{
+                  color: "#08005F",
 
-       <OrderTextBox>
-        <NumberText>
-          01
-        </NumberText>
-        <OrderContent>
-          <Typography sx={{color: '#08005F',
+                  fontFamily: "Inter",
+                  fontSize: "22px",
+                  fontStyle: "normal",
+                  fontWeight: "600",
+                  lineHeight: "normal",
+                }}
+              >
+                Select your Store
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#828282",
+                  marginTop: "5%",
+                  fontFamily: "Questrial",
+                  fontSize: "14px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "166.023%",
+                }}
+              >
+                from your morning coffee needs to snacks
+                <br /> for your late night binge.
+              </Typography>
+            </OrderContent>
+          </OrderTextBox>
 
-fontFamily: 'Inter',
-fontSize: '22px',
-fontStyle: 'normal',
-fontWeight: '600',
-lineHeight: 'normal',}}>
-          Select your Store
-          </Typography>
-          <Typography sx={{color: '#828282',
-          marginTop:'5%',
-    fontFamily:'Questrial',
-    fontSize:'14px',
-    fontStyle:'normal',
-    fontWeight:'400',
-    lineHeight:'166.023%',}}>
-          from your morning coffee needs to snacks<br/> for your late night binge.
-          </Typography>
-        </OrderContent>
-       </OrderTextBox>
+          <OrderTextBox>
+            <NumberText>01</NumberText>
+            <OrderContent>
+              <Typography
+                sx={{
+                  color: "#08005F",
 
-       <OrderTextBox>
-        <NumberText>
-          01
-        </NumberText>
-        <OrderContent>
-          <Typography sx={{color: '#08005F',
+                  fontFamily: "Inter",
+                  fontSize: "22px",
+                  fontStyle: "normal",
+                  fontWeight: "600",
+                  lineHeight: "normal",
+                }}
+              >
+                Select your Store
+              </Typography>
+              <Typography
+                sx={{
+                  color: "#828282",
+                  marginTop: "5%",
+                  fontFamily: "Questrial",
+                  fontSize: "14px",
+                  fontStyle: "normal",
+                  fontWeight: "400",
+                  lineHeight: "166.023%",
+                }}
+              >
+                from your morning coffee needs to snacks
+                <br /> for your late night binge.
+              </Typography>
+            </OrderContent>
+          </OrderTextBox>
+        </div>
+      </div>
+      <hr style={{ border: "1px solid", color: "blue", marginTop: "58px" }} />
+    </div>
+    // </Box>
+  );
+};
 
-fontFamily: 'Inter',
-fontSize: '22px',
-fontStyle: 'normal',
-fontWeight: '600',
-lineHeight: 'normal',}}>
-          Select your Store
-          </Typography>
-          <Typography sx={{color: '#828282',
-          marginTop:'5%',
-    fontFamily:'Questrial',
-    fontSize:'14px',
-    fontStyle:'normal',
-    fontWeight:'400',
-    lineHeight:'166.023%',}}>
-          from your morning coffee needs to snacks<br/> for your late night binge.
-          </Typography>
-        </OrderContent>
-       </OrderTextBox>
-       </Box>
-       </Box>
-
-       <hr  style={{border:'1px solid',
-    color:'blue',
-    marginTop:'58px',}}/>
-    </Box>
-  )
-}
-
-export default Order_content
-{/* <Box>
-
-<Box>
-    <Typography>
-        01
-    </Typography>
-</Box>
-<Box>
-
-<Box>
-    <Typography>
-    Select your Store
-    </Typography>
-    <Typography>
-    from your morning coffee needs to snacks for your late night binge.
-    </Typography>
-</Box>
-
-</Box>
-<Box>
-    <Typography>
-        01
-    </Typography>
-</Box>
-<Box>
-<Box>
-    <Typography>
-    Select your Store
-    </Typography>
-    <Typography>
-    from your morning coffee needs to snacks for your late night binge.
-    </Typography>
-</Box>
-
-</Box>
-
-</Box> */}
+export default Order_content;
